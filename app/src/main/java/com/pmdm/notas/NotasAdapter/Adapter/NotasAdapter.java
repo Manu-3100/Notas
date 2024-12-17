806338bc57bd972d9fb7bd1483d0ee9b0fbf87e7
arodomgz <aroatorresdominguez@gmail.com>
2024-12-17 13:11:17 +0100

    notasAdapter e clase nota.java

--- app/src/main/java/com/pmdm/notas/NotasAdapter/Adapter/NotasAdapter.java
+++ app/src/main/java/com/pmdm/notas/NotasAdapter/Adapter/NotasAdapter.java
@@ -1,4 +1,81 @@
 package com.pmdm.notas.NotasAdapter.Adapter;
 
-public class NotasAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pmdm.notas.NotasAdapter.Entities.Nota;
+import com.pmdm.notas.R;
+
+import java.util.List;
+
+public class NotasAdapter extends RecyclerView.Adapter<NotasAdapter.NotasAdapterHolder> {
+
+    private List<Nota> lNota;
+    private Context cxt;
+    private final OnItemClickListener notaListener;
+
+    public NotasAdapter(List<Nota> dataSet, Context contexto, OnItemClickListener listener) {
+        this.lNota = dataSet;
+        this.cxt = contexto;
+        this.notaListener = listener;
+    }
+
+    @NonNull
+    public NotasAdapterHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.nota,parent,false);
+        return new NotasAdapterHolder(inflate);
+    }
+
+    public void onBindViewHolder(@NonNull NotasAdapterHolder holder, int position){
+        holder.mitem = lNota.get(position);
+
+        holder.tvTitulo.setText(holder.mitem.getTitulo());
+        holder.tvData.setText(holder.mitem.getData());
+        holder.tvModulo.setText(holder.mitem.getModulo());
+
+        holder.itemView.setOnClickListener(view -> {
+            if(notaListener != null){
+                notaListener.onItemClick(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount(){
+        return lNota.size();
+    }
+
+    public class NotasAdapterHolder extends RecyclerView.ViewHolder {
+        public final View vista;
+        public final TextView tvTitulo;
+        public final TextView tvData;
+        public final TextView tvModulo;
+        public final ImageView ivImaxe;
+        public Nota mitem = null;
+
+        public NotasAdapterHolder(View view){
+            super(view);
+            vista = view;
+            ivImaxe = (ImageView) view.findViewById(R.id.imaxe);
+            tvTitulo = (TextView) view.findViewById(R.id.tvTitulo);
+            tvData = (TextView) view.findViewById(R.id.tvData);
+            tvModulo = (TextView) view.findViewById(R.id.tvModulo);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "\t" + tvTitulo.getText() + "\t";
+        }
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
 }
--- app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/Nota.java
+++ app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/Nota.java
@@ -1,4 +1,54 @@
 package com.pmdm.notas.NotasAdapter.Entities;
 
 public class Nota {
+    private String titulo;
+    private String data;
+    private String modulo;
+    private String imaxe;
+
+    public Nota(String titulo, String data, String modulo) {
+        this.titulo = titulo;
+        this.data = data;
+        this.modulo = modulo;
+    }
+
+    public Nota(String titulo, String imaxe, String modulo, String data) {
+        this.titulo = titulo;
+        this.imaxe = imaxe;
+        this.modulo = modulo;
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getModulo() {
+        return modulo;
+    }
+
+    public void setModulo(String modulo) {
+        this.modulo = modulo;
+    }
+
+    public String getImaxe() {
+        return imaxe;
+    }
+
+    public void setImaxe(String imaxe) {
+        this.imaxe = imaxe;
+    }
+
 }
