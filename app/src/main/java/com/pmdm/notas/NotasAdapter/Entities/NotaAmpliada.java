d5257d7ae3bc91f16aaf1b17599f2310b55f176a
arodomgz <aroatorresdominguez@gmail.com>
2024-12-18 13:11:20 +0100

    bule botons

--- app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/NotaAmpliada.java
+++ app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/NotaAmpliada.java
@@ -2,7 +2,9 @@ package com.pmdm.notas.NotasAdapter.Entities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,8 @@ public class NotaAmpliada extends AppCompatActivity {
     private EditText etTitulo;
     private EditText etData;
     private EditText etModulo;
+    private Button btGardar;
+    private Button btCancelar;
 
     int position = 0;
 
@@ -32,10 +36,20 @@ public class NotaAmpliada extends AppCompatActivity {
         etTitulo = (EditText) findViewById(R.id.cambiarTitulo);
         etData = (EditText) findViewById(R.id.cambiarData);
         etModulo = (EditText) findViewById(R.id.cambiarModulo);
+        btGardar = (Button) findViewById(R.id.btGardar);
+        btCancelar = (Button) findViewById(R.id.btCancelar);
 
-        Intent intent = getIntent();
-       // position = intent.getExtras().getInt(NotasActivity.position);
-       // etTitulo.setText(intent.getExtras()
-       //         .getString(NotasActivity.notasList.get(position).getTitulo()));
+        Intent intent = getIntent(); //recibir os datos
+        position = intent.getExtras().getInt(NotasActivity.position);
+    }
+
+    public void gardarClick(){
+        Intent datos_volta = new Intent();
+        if(etTitulo.getText() != null){
+            NotasActivity.notasList.get(position).getTitulo() = etTitulo.getText();
+            datos_volta.putExtra()
+        } else {
+            Toast.makeText(getContext(), "O campo non pode estar vacío", Toast.LENGTH_SHORT).show();
+        }
     }
 }
