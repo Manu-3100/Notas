d9ff20ce7bedd46cba3b2f7fb4092d5999d8edd4
arodomgz <aroatorresdominguez@gmail.com>
2024-12-18 12:37:44 +0100

    clase nota ampliada

--- app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/NotaAmpliada.java
+++ app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/NotaAmpliada.java
@@ -1,6 +1,26 @@
 package com.pmdm.notas.NotasAdapter.Entities;
 
-public class NotaAmpliada {
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class NotaAmpliada extends AppCompatActivity {
+
+    private EditText etTitulo;
+    private EditText etData;
+    private EditText etModulo;
+
+    private List<Nota> lNotas;
+
+        
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
+    }
 }
