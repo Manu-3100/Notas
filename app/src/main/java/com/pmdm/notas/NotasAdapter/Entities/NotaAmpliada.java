03ffc39caa10a7eaa7de98cc8ce9499fbbcad3a3
arodomgz <aroatorresdominguez@gmail.com>
2024-12-18 12:51:05 +0100

    nota ampiada.java

--- app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/NotaAmpliada.java
+++ app/src/main/java/com/pmdm/notas/NotasAdapter/Entities/NotaAmpliada.java
@@ -1,10 +1,14 @@
 package com.pmdm.notas.NotasAdapter.Entities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.pmdm.notas.NotasAdapter.Activities.NotasActivity;
+import com.pmdm.notas.R;
 
 import java.util.List;
 
@@ -13,14 +17,24 @@ public class NotaAmpliada extends AppCompatActivity {
     private EditText etTitulo;
     private EditText etData;
     private EditText etModulo;
+    private int position;
 
     private List<Nota> lNotas;
 
-        
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.id.nota_ampliada);
 
+        etTitulo = (EditText) findViewById(R.id.cambiarTitulo);
+        etData = (EditText) findViewById(R.id.cambiarData);
+        etModulo = (EditText) findViewById(R.id.cambiarModulo);
+
+        Intent intent = getIntent();
+        position = intent.getExtras().getInt(NotasActivity.position);
+        etTitulo.setText(intent.getExtras()
+                .getString(NotasActivity.notasList.get(position).getTitulo()));
     }
 }
